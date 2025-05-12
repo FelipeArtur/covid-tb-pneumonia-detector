975660bc63cf415b1944656165585b99991abd0a
Rafael Miguez <rafael0miguez@gmail.com>
2025-05-12 20:17:24 -0300

    [ATT] Comentario em preverimagem
    - Adicionado uma explicação breve para a função prever_imagem()

--- preverimagem.py
+++ preverimagem.py
@@ -1,3 +1,5 @@
 from Leitor import prever_imagem
 
-prever_imagem("C:/Ragnarok/covid-tb-pneumonia-detector/dataset/TEST/COVID/Tuberculosis-426.png")
+
+# Quando for prever outro tipo de doença, alterar a pasta lida, ex: TEST/PNEUMONIA para TEST/NORMAL
+prever_imagem("C:/Ragnarok/covid-tb-pneumonia-detector/dataset/TEST/NORMAL/Normal-3161.png")
