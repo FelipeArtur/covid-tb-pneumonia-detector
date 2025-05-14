dad168b7932d68d496704f5f9b8e2298c7a2d45c
Felipe Lima <felipe.artur.ml@gmail.com>
2025-05-14 18:34:38 -0300

    Remove unused model and prediction scripts

--- preverimagem.py
+++
@@ -1,5 +0,0 @@
-from Leitor import prever_imagem
-
-
-# Quando for prever outro tipo de doença, alterar a pasta lida, ex: TEST/PNEUMONIA para TEST/NORMAL
-prever_imagem("C:/Ragnarok/covid-tb-pneumonia-detector/dataset/TEST/NORMAL/Normal-3161.png")
